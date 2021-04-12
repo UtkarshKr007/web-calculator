3c2c994d0d0af117ac37971468190b9656475732
Utkarsh Kumar <kumar375@morris.umn.edu>
2021-04-12 09:30:43 -0500

    Initialize client with creat-react-app & clean-up
    Removed unneded packages that come with create-react-app and deleted the
    css files since we will be using Material UI.

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          This will be a calculator app
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
